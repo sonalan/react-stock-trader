54bc7c64db5459244a757c3db7bd91a8214bb8de
sonalan <sinan@onalan.net>
2020-11-29 23:26:24 -0800

    implemented header, content and footer components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from  './componets/header';
+import Content from  './componets/content';
+import Footer from  './componets/footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Content />
+      <Footer />
     </div>
   );
 }
